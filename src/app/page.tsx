878a42a7490b4f8129b11058415b62f33b6fcbaa
kagisho96 <kagishopitsi@gmail.com>
2024-01-25 12:12:13 +0200

    changing h1 in hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -184,8 +184,8 @@ export default async function Home() {
     <>
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
-          <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Award-winning development studio based in Denmark.
+          <h1 className="font-display text-5xl tracking-tight [text-wrap:balance] sm:text-6xl">
+            <span className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">IOT Masters:</span> Hackathon champs excelling in data science, ML innovation.
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
             We are a development studio working at the intersection of design
