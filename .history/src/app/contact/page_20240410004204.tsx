e5e5c9a1b299a82d31854b5c5ddbc673c3deb313
kagisho96 <kagishopitsi@gmail.com>
2024-04-15 11:29:34 +0200

    fix: page

--- .history/src/app/contact/page_20240410004204.tsx
+++ .history/src/app/contact/page_20240410004204.tsx
@@ -1,56 +1,54 @@
 import Link from "next/link";
 import { type Metadata } from "next";
+import { useId } from "react";
 import { Border } from "@/components/Border";
+import { Button } from "@/components/Button";
 import { Container } from "@/components/Container";
 import { FadeIn } from "@/components/FadeIn";
 import { Offices } from "@/components/Offices";
 import { PageIntro } from "@/components/PageIntro";
 import { SocialMedia } from "@/components/SocialMedia";
 
-// import { useId } from "react";
+function TextInput({
+  label,
+  ...props
+}: React.ComponentPropsWithoutRef<'input'> & { label: string }) {
+  let id = useId()
 
-// import { Button } from "@/components/Button";
+  return (
+    <div className="group relative z-0 transition-all focus-within:z-10">
+      <input
+        type="text"
+        id={id}
+        {...props}
+        placeholder=" "
+        className="peer block w-full border border-neutral-300 bg-transparent px-6 pb-4 pt-12 text-base/6 text-neutral-950 ring-4 ring-transparent transition focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5 group-first:rounded-t-2xl group-last:rounded-b-2xl"
+      />
+      <label
+        htmlFor={id}
+        className="pointer-events-none absolute left-6 top-1/2 -mt-3 origin-left text-base/6 text-neutral-500 transition-all duration-200 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-semibold peer-focus:text-neutral-950 peer-[:not(:placeholder-shown)]:-translate-y-4 peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:text-neutral-950"
+      >
+        {label}
+      </label>
+    </div>
+  )
+}
 
-// function TextInput({
-//   label,
-//   ...props
-// }: React.ComponentPropsWithoutRef<'input'> & { label: string }) {
-//   let id = useId()
-
-//   return (
-//     <div className="group relative z-0 transition-all focus-within:z-10">
-//       <input
-//         type="text"
-//         id={id}
-//         {...props}
-//         placeholder=" "
-//         className="peer block w-full border border-neutral-300 bg-transparent px-6 pb-4 pt-12 text-base/6 text-neutral-950 ring-4 ring-transparent transition focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5 group-first:rounded-t-2xl group-last:rounded-b-2xl"
-//       />
-//       <label
-//         htmlFor={id}
-//         className="pointer-events-none absolute left-6 top-1/2 -mt-3 origin-left text-base/6 text-neutral-500 transition-all duration-200 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-semibold peer-focus:text-neutral-950 peer-[:not(:placeholder-shown)]:-translate-y-4 peer-[:not(:placeholder-shown)]:scale-75 peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:text-neutral-950"
-//       >
-//         {label}
-//       </label>
-//     </div>
-//   )
-// }
-
-// function RadioInput({
-//   label,
-//   ...props
-// }: React.ComponentPropsWithoutRef<'input'> & { label: string }) {
-//   return (
-//     <label className="flex gap-x-3">
-//       <input
-//         type="radio"
-//         {...props}
-//         className="h-6 w-6 flex-none appearance-none rounded-full border border-neutral-950/20 outline-none checked:border-[0.5rem] checked:border-neutral-950 focus-visible:ring-1 focus-visible:ring-neutral-950 focus-visible:ring-offset-2"
-//       />
-//       <span className="text-base/6 text-neutral-950">{label}</span>
-//     </label>
-//   )
-// }
+function RadioInput({
+  label,
+  ...props
+}: React.ComponentPropsWithoutRef<'input'> & { label: string }) {
+  return (
+    <label className="flex gap-x-3">
+      <input
+        type="radio"
+        {...props}
+        className="h-6 w-6 flex-none appearance-none rounded-full border border-neutral-950/20 outline-none checked:border-[0.5rem] checked:border-neutral-950 focus-visible:ring-1 focus-visible:ring-neutral-950 focus-visible:ring-offset-2"
+      />
+      <span className="text-base/6 text-neutral-950">{label}</span>
+    </label>
+  )
+}
 
 // function ContactForm() {
 //   return (
@@ -113,8 +111,8 @@ function ContactDetails() {
         </h2>
         <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
           {[
-            ['Pitsi', 'kagishopitsi@gmail.com'],
-            ['Mphaka', 'jjmphaka@gmail.com'],
+            ['Pitsi', 'kagishopitsi@gma.com'],
+            ['Press', 'press@studioagency.com'],
           ].map(([label, email]) => (
             <div key={email}>
               <dt className="font-semibold text-neutral-950">{label}</dt>
